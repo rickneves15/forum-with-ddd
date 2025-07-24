55500141f1f69f006d7026eedf14c7f159701303
Richard Neves <ricknevesbc@icloud.com>
2025-07-24 20:34:14 -0300

    feat: Implement DeleteAnswer use case and repository methods
    Adds DeleteAnswerUseCase for handling answer deletion, including validation for author permissions. Updates AnswerRepository interface with findById and delete methods, and provides an in-memory implementation for testing. Includes unit tests to ensure correct functionality of the delete operation.

---
+++ src/domain/forum/application/use-cases/delete-answer.spec.ts
@@ -0,0 +1,50 @@
+import { makeAnswer } from 'test/factories/make-answer'
+import { InMemoryAnswerRepository } from 'test/repositories/in-memory-answer-repository'
+import { UniqueEntityID } from '~/core/entities/unique-entity-id'
+import { DeleteAnswerUseCase } from './delete-answer'
+
+let inMemoryAnswerRepository: InMemoryAnswerRepository
+let sut: DeleteAnswerUseCase
+
+describe('Delete Answer', () => {
+  beforeEach(() => {
+    inMemoryAnswerRepository = new InMemoryAnswerRepository()
+    sut = new DeleteAnswerUseCase(inMemoryAnswerRepository)
+  })
+
+  test('it should be able to delete a answer', async () => {
+    const newAnswer = makeAnswer(
+      {
+        authorId: new UniqueEntityID('author-1'),
+      },
+      new UniqueEntityID('answer-1')
+    )
+
+    await inMemoryAnswerRepository.create(newAnswer)
+
+    await sut.execute({
+      answerId: 'answer-1',
+      authorId: 'author-1',
+    })
+
+    expect(inMemoryAnswerRepository.items).toHaveLength(0)
+  })
+
+  test('it should not be able to delete a answer from another user', async () => {
+    const newAnswer = makeAnswer(
+      {
+        authorId: new UniqueEntityID('author-1'),
+      },
+      new UniqueEntityID('answer-2')
+    )
+
+    await inMemoryAnswerRepository.create(newAnswer)
+
+    expect(() => {
+      return sut.execute({
+        answerId: 'answer-1',
+        authorId: 'author-1',
+      })
+    }).rejects.toBeInstanceOf(Error)
+  })
+})
---
+++ src/domain/forum/application/use-cases/delete-answer.ts
@@ -0,0 +1,31 @@
+import type { AnswerRepository } from '~/domain/forum/application/repositories/answer-repository'
+
+interface DeleteAnswerUseCaseRequest {
+  answerId: string
+  authorId: string
+}
+
+type DeleteAnswerUseCaseResponse = {}
+
+export class DeleteAnswerUseCase {
+  constructor(private answerRepository: AnswerRepository) {}
+
+  async execute({
+    answerId,
+    authorId,
+  }: DeleteAnswerUseCaseRequest): Promise<DeleteAnswerUseCaseResponse> {
+    const answer = await this.answerRepository.findById(answerId)
+
+    if (!answer) {
+      throw new Error('Answer not found.')
+    }
+
+    if (authorId !== answer.authorId.toString()) {
+      throw new Error('Not allowed.')
+    }
+
+    await this.answerRepository.delete(answer)
+
+    return {}
+  }
+}
