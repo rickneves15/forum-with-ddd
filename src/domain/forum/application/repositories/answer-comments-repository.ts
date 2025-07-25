6b3fec7518205d261ce46c49f5ff615664f9b6fb
Richard Neves <ricknevesbc@icloud.com>
2025-07-25 16:07:27 -0300

    feat(forum): Implement delete functionality for answer and question comments
    Adds DeleteAnswerCommentUseCase and DeleteQuestionCommentUseCase to enable users to delete their comments on answers and questions. Updates the corresponding repositories with findById and delete methods for managing comments. Includes in-memory implementations and unit tests to verify functionality, enhancing comment management within the forum.

--- src/domain/forum/application/repositories/answer-comments-repository.ts
+++ src/domain/forum/application/repositories/answer-comments-repository.ts
@@ -1,5 +1,7 @@
 import type { AnswerComment } from '~/domain/forum/enterprise/entities/answer-comment'
 
 export interface AnswerCommentsRepository {
+  findById(id: string): Promise<AnswerComment | null>
   create(answerComments: AnswerComment): Promise<void>
+  delete(answerComment: AnswerComment): Promise<void>
 }
---
+++ src/domain/forum/application/use-cases/delete-answer-comment.spec.ts
@@ -0,0 +1,42 @@
+import { makeAnswerComment } from 'test/factories/make-answer-comment'
+import { InMemoryAnswerCommentsRepository } from 'test/repositories/in-memory-answer-comments-repository'
+import { UniqueEntityID } from '~/core/entities/unique-entity-id'
+import { DeleteAnswerCommentUseCase } from './delete-answer-comment'
+
+let inMemoryAnswerCommentsRepository: InMemoryAnswerCommentsRepository
+let sut: DeleteAnswerCommentUseCase
+
+describe('Delete Answer Comment', () => {
+  beforeEach(() => {
+    inMemoryAnswerCommentsRepository = new InMemoryAnswerCommentsRepository()
+    sut = new DeleteAnswerCommentUseCase(inMemoryAnswerCommentsRepository)
+  })
+
+  test('it should be able to delete a question comment', async () => {
+    const answerComment = makeAnswerComment()
+
+    await inMemoryAnswerCommentsRepository.create(answerComment)
+
+    await sut.execute({
+      authorId: answerComment.authorId.toString(),
+      answerCommentId: answerComment.id.toString(),
+    })
+
+    expect(inMemoryAnswerCommentsRepository.items).toHaveLength(0)
+  })
+
+  test('it should not be able to delete another user question comment', async () => {
+    const answerComment = makeAnswerComment({
+      authorId: new UniqueEntityID('author-1'),
+    })
+
+    await inMemoryAnswerCommentsRepository.create(answerComment)
+
+    await expect(() =>
+      sut.execute({
+        authorId: 'author-2',
+        answerCommentId: answerComment.id.toString(),
+      })
+    ).rejects.toBeInstanceOf(Error)
+  })
+})
